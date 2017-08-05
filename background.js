737c56a1c61d2d2dba40e03c276369efdbd45a36
wildskyf <poppin.wildsky@gmail.com>
2017-08-05 13:51:01 +0800

    make display rule stricter

--- background.js
+++ background.js
@@ -1,5 +1,10 @@
 browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
   if (!/.*google.com.*/.test(tab.url)) return;
+    browser.tabs.sendMessage(
+      tab.id, 'cancel-two-col'
+    );
+    return;
+  }
   browser.pageAction.show(tab.id);
 });
 
--- content-script.js
+++ content-script.js
@@ -2,4 +2,8 @@ browser.runtime.onMessage.addListener( msg => {
   if (msg == 'switch-two-col') {
     document.querySelector('body').classList.toggle('no-two-col');
   }
+  else if (msg == 'cancel-two-col') {
+    document.querySelector('body').classList.add('no-two-col');
+  }
 });
+
