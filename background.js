0a10c0c015e2741c6aaa1f73fa291b5d617ce1a5
wildskyf <poppin.wildsky+gh@gmail.com>
2018-03-11 15:39:52 +0800

    refactor & make cancel command not run when tab not completed

--- background.js
+++ background.js
@@ -1,19 +1,17 @@
-browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
-  if (
-    !/https:\/\/.*startpage.com\/do\/search/.test(tab.url) && // for startpage
-    !/.*google.*\/search/.test(tab.url) ||
-    /.*google.*\/search\?.*tbm=lcl/.test(tab.url)  // embeded shops
-  ) {
-    browser.tabs.sendMessage(
-      tab.id, 'cancel-two-col'
-    );
+browser.tabs.onUpdated.addListener( (id, changeInfo, tab) => {
+  var isStartPage = url => /https:\/\/.*startpage.com\/do\/search/.test(url);
+  var isGSearch = url => /.*google.*\/search/.test(tab.url)
+  var isEmbededShop = url => /.*google.*\/search\?.*tbm=lcl/.test(url);
+
+  if (tab.status !== "complete") return;
+
+  if ( (!isStartPage(tab.url) && !isGSearch(tab.url)) || isEmbededShop(tab.url)) {
+    browser.tabs.sendMessage(tab.id, 'cancel-two-col');
     return;
   }
   browser.pageAction.show(tab.id);
 });
 
-browser.pageAction.onClicked.addListener(tab => {
-  browser.tabs.sendMessage(
-    tab.id, 'switch-two-col'
-  )
+browser.pageAction.onClicked.addListener( tab => {
+  browser.tabs.sendMessage(tab.id, 'switch-two-col')
 });
