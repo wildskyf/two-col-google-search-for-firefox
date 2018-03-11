9d60c5e2df85cccd3ac2d435629548af1890e218
wildskyf <poppin.wildsky+gh@gmail.com>
2018-03-11 15:08:04 +0800

    support websites like www.google.fr

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
   if (
     !/https:\/\/.*startpage.com\/do\/search/.test(tab.url) && // for startpage
-    !/.*google.com.*\/search/.test(tab.url) ||
-    /.*google.com.*\/search\?.*tbm=lcl/.test(tab.url)  // embeded shops
+    !/.*google.*\/search/.test(tab.url) ||
+    /.*google.*\/search\?.*tbm=lcl/.test(tab.url)  // embeded shops
   ) {
     browser.tabs.sendMessage(
       tab.id, 'cancel-two-col'
